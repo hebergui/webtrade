73dafec31cf5a2ebb5de0063efe3a7847559b6f3
Guillaume Hébert <hebergui.utc@gmail.com>
2020-11-24 12:05:03 +0100

    Update urls.py

--- dashboard/api/urls.py
+++ dashboard/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import *
+from . import views
 
 
 router = routers.DefaultRouter()
-router.register(r'companies', CompanyViewSet)
-router.register(r'indicators', IndicatorViewSet)
+router.register(r'companies', views.CompanyViewSet)
+router.register(r'indicators', views.IndicatorViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
