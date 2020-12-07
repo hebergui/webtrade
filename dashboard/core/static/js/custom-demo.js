99a1f74fc5978e18d8115f77c075fd4a65a6d46c
hebergui <hebergui.utc@gmail.com>
2020-12-07 18:40:33 +0100

    adjust ui

--- dashboard/core/static/js/custom-demo.js
+++ dashboard/core/static/js/custom-demo.js
@@ -1,11 +1,13 @@
-(function($) {
-  "use strict"; // Start of use strict
-
+$(document).ready(function() {
   // Toggle the side navigation
   $("#sidebarToggle").on('click', function(e) {
-    e.preventDefault();
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
+  });
+
+  $(".nav-link").click(function () {
+    $(".nav-item").removeClass("active");
+
   });
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
@@ -14,7 +16,6 @@
       var e0 = e.originalEvent,
         delta = e0.wheelDelta || -e0.detail;
       this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-      e.preventDefault();
     }
   });
 
@@ -34,7 +35,6 @@
     $('html, body').stop().animate({
       scrollTop: ($($anchor.attr('href')).offset().top)
     }, 1000, 'easeInOutExpo');
-    event.preventDefault();
   });
 
-})(jQuery); // End of use strict
+});
